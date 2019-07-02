0210301c9ee816daf5a4798b1b91b176ba283e16
kartik009 <lakhanikartik9@gmail.com>
2019-07-02 18:08:12 +0530

    Initial state

---
+++ FleetTrackDemo1/FleetTrackDemo1/FleetTrackDemo1/Model/Vehical.cs
@@ -0,0 +1,7 @@
+﻿namespace FleetTrackDemo1.Model
+{
+    public class Vehical
+    {
+        public string RCNo { get; set; }
+    }
+}
---
+++ FleetTrackDemo1/FleetTrackDemo1/FleetTrackDemo1/ViewModel/FleetListViewModel.cs
@@ -0,0 +1,26 @@
+﻿using FleetTrackDemo1.Model;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace FleetTrackDemo1.ViewModel
+{
+    public class FleetListViewModel
+    {
+        public ObservableCollection<Vehical> Fleets { get; set; }
+
+        public List<Vehical> v = new List<Vehical>()
+        {
+            new Vehical{   RCNo = "Gj 05 LK 5360" },
+            new Vehical{RCNo = "Gj 01 LM 5860"}
+        };
+
+        public FleetListViewModel()
+        {
+            Fleets = new ObservableCollection<Vehical>();
+            foreach (Vehical vehical in v)
+                Fleets.Add(vehical);
+
+        }
+
+    }
+}
